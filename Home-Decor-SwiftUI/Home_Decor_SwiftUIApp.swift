f4db055ce2fb14e0cc418932ddec8be098d9988f
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-01-25 12:14:57 -0800

    Initial Commit

---
+++ Home-Decor-SwiftUI/Home_Decor_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Decor_SwiftUIApp.swift
+//  Home-Decor-SwiftUI
+//
+//  Created by ReetDhillon on 2024-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Home_Decor_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
